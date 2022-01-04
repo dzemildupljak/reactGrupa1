1c2a8732085fe8f2aa2d6612900f2aaa88515081
dzemildupljak <dzemildupljak4795@gmail.com>
2022-01-04 21:09:52 +0100

    homework for checkbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [newtask, setNewtask] = useState({
+    title: "",
+    isDone: false,
+  });
+  const [tasks, setTasks] = useState([]);
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+
+    setNewtask({ ...newtask, title: e.target.value });
+  };
+
+  const addNewTask = () => {
+    if (newtask.title !== "") {
+      setTasks([...tasks, newtask]);
+      setNewtask({ title: "" });
+    }
+  };
+
+  const removeLastTask = () => {
+    setTasks(
+      tasks.filter((t, i) => {
+        if (i !== tasks.length - 1) {
+          return t;
+        }
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <label htmlFor="name-input">Unesite ime </label>
+      <input
+        name="name-input"
+        type="text"
+        placeholder="Unesite ime"
+        value={newtask.title}
+        onChange={(e) => {
+          handleInputChange(e);
+        }}
+      />
+      <button onClick={addNewTask}> add new task</button>
+
+      <ul>
+        {tasks.map((t, i) => {
+          return (
+            <li key={i}>
+              <span>{t.title}</span>
+              {/* TODO change checkbox value onClick */}
+              <input type="checkbox" value={t.isDone} />
+            </li>
+          );
+        })}
+      </ul>
+
+      <button onClick={removeLastTask}>remove last task</button>
     </div>
   );
 }
